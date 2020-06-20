06df69bc5c92cb02d00e0885743f3e1c6ac25106
sethGu <602841235@qq.com>
2020-06-21 05:00:23 +1000

    first push

--- MLPrac7/prac7.bat
+++
@@ -1,4 +0,0 @@
-set PATH=C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64;C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts;%PATH%
-pip install -U pip
-pip install tensorflow==1.3 jupyter[notebook]
-jupyter notebook
